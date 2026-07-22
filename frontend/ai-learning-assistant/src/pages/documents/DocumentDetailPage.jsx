bca63f34790e6383bb018abdc2d077626f105223
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2026-07-22 12:35:43 +0100

    feat: add QuizManager and QuizCard components for managing quizzes in DocumentDetailPage

--- frontend/ai-learning-assistant/src/pages/documents/DocumentDetailPage.jsx
+++ frontend/ai-learning-assistant/src/pages/documents/DocumentDetailPage.jsx
@@ -9,6 +9,7 @@ import Tabs from '../../components/common/Tabs';
 import ChatInterface from '../../components/chat/ChatInterface';
 import AIActions from '../../components/ai/AIActions';
 import FlashcardManager from '../../components/flashcards/FlashcardManager';
+import QuizManager from '../../components/quizzes/QuizManager';
 
 const DocumentDetailPage = () => {
 
@@ -88,7 +89,7 @@ const DocumentDetailPage = () => {
   }
 
   const renderQuizzesTab = () =>{
-    return "renderQuizzesTab"
+    return <QuizManager documentId = {id} />
   }
 
   const tabs = [
